4de525c3b01741333de5377d78a97552f9e4c8e4
Нечипорук Андрей <nechiporuk@awem.local>
2020-05-22 22:45:22 +0300

    'initial'

---
+++ Assets/Scripts/MenuScene/StartBtn.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartBtn : MonoBehaviour
+{
+    private const string gameSceneName = "GameScene";
+
+    public Scene GetScene { get; set; }
+
+    public void OnClick()
+    {
+        ChangeScene(gameSceneName);
+    }
+
+    private void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
